b3d9ef5c2baaed8322bdb4eaeed811e635015440
Chao Li <chaoli0394@gmail.com>
2022-05-01 16:34:45 +0900

    B: forget using .dissolve() in some POI; revise Py 19_DW; done

--- 01_PyCode/19_DW_PointOfInterestsDistance_v1.py
+++ 01_PyCode/19_DW_PointOfInterestsDistance_v1.py
@@ -18,6 +18,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 import os
 import time
 
+firstTimeRunCode = False
+
 ### extraction
 coords_extration = gpd.read_file("F:/17_Article/01_Data/00_mesh/mesh_center_point.shp")
 
@@ -61,66 +63,70 @@ coords_extration = coords_extration.sjoin_nearest(hospitalShapeFile, distance_co
 
 ### park
 aimFolder = "F:\\17_Article\\01_Data\\07_PointOfInterest\\park"
-os.mkdir(aimFolder + "\\temp")
-
-### park
-### change the default path
-chromeOptions = webdriver.ChromeOptions()
-prefs = {"download.default_directory" : aimFolder + "\\temp"}
-chromeOptions.add_experimental_option("prefs",prefs)
-
-driver = webdriver.Chrome(ChromeDriverManager(version="100.0.4896.60").install(), chrome_options = chromeOptions)
-
-locationService = "https://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-P13.html"
-driver.get(locationService)
-
-# /html/body/main/div/table/tbody/tr[2]/td[6]/a
-# /html/body/main/div/table/tbody/tr[3]/td[6]/a
-# ...
-# /html/body/main/div/table/tbody/tr[48]/td[6]/a
-
-### download the zip file
-beginIndex = 2
-
-while beginIndex < 49:
-    driver.find_element_by_xpath("/html/body/main/div/table/tbody/tr[" + str(beginIndex) + "]/td[6]/a").click()
-    time.sleep(2)
+if firstTimeRunCode:
+    os.mkdir(aimFolder + "\\temp")
     
-    driver.switch_to.alert.accept()
-    time.sleep(2)
+    ### park
+    ### change the default path
+    chromeOptions = webdriver.ChromeOptions()
+    prefs = {"download.default_directory" : aimFolder + "\\temp"}
+    chromeOptions.add_experimental_option("prefs",prefs)
     
-    beginIndex += 1
-
-driver.quit()
-
-### unzip downloaded files
-fileList = glob.glob(aimFolder + "\\temp" + "\\*.zip")
-for filename in fileList:
-    with zipfile.ZipFile(filename, "r") as zip_ref:
-        zip_ref.extractall(aimFolder + "\\temp")
-
-### read the shapefile        
-shapeFileList = glob.glob(aimFolder + "\\temp" + "\\*.shp")
-gdpFileArray = []
-for shapeFileName in shapeFileList:
-    gdpFile = gpd.read_file(shapeFileName)
-    gdpFileArray.append(gdpFile)
+    driver = webdriver.Chrome(ChromeDriverManager(version="100.0.4896.60").install(), chrome_options = chromeOptions)
     
-gdf = gpd.GeoDataFrame(pd.concat(gdpFileArray))
-gdf = gdf[['geometry']]
-gdf.to_file(aimFolder + "\\MergedPark.shp")
-
-### remove the temp folder 
-for root, dirs, files in os.walk(aimFolder + "\\temp", topdown=False):
-    for name in files:
-        os.remove(os.path.join(root, name))
-    for name in dirs:
-        os.rmdir(os.path.join(root, name))
-os.rmdir(aimFolder + "\\temp")
+    locationService = "https://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-P13.html"
+    driver.get(locationService)
+    
+    # /html/body/main/div/table/tbody/tr[2]/td[6]/a
+    # /html/body/main/div/table/tbody/tr[3]/td[6]/a
+    # ...
+    # /html/body/main/div/table/tbody/tr[48]/td[6]/a
+    
+    ### download the zip file
+    beginIndex = 2
+    
+    while beginIndex < 49:
+        driver.find_element_by_xpath("/html/body/main/div/table/tbody/tr[" + str(beginIndex) + "]/td[6]/a").click()
+        time.sleep(2)
+        
+        driver.switch_to.alert.accept()
+        time.sleep(2)
+        
+        beginIndex += 1
+    
+    driver.quit()
+    
+    ### unzip downloaded files
+    fileList = glob.glob(aimFolder + "\\temp" + "\\*.zip")
+    for filename in fileList:
+        with zipfile.ZipFile(filename, "r") as zip_ref:
+            zip_ref.extractall(aimFolder + "\\temp")
+    
+    ### read the shapefile        
+    shapeFileList = glob.glob(aimFolder + "\\temp" + "\\*.shp")
+    gdpFileArray = []
+    for shapeFileName in shapeFileList:
+        gdpFile = gpd.read_file(shapeFileName)
+        gdpFileArray.append(gdpFile)
+        
+    gdf = gpd.GeoDataFrame(pd.concat(gdpFileArray))
+    gdf = gdf[['geometry']]
+    gdf.to_file(aimFolder + "\\MergedPark.shp")
+    
+    ### remove the temp folder 
+    for root, dirs, files in os.walk(aimFolder + "\\temp", topdown=False):
+        for name in files:
+            os.remove(os.path.join(root, name))
+        for name in dirs:
+            os.rmdir(os.path.join(root, name))
+    os.rmdir(aimFolder + "\\temp")
+else:
+    gdf = gpd.read_file(aimFolder + "\\MergedPark.shp")
 
 gdf.crs
 gdf = gdf.set_crs(epsg = 6668)
 gdf = gdf.to_crs(epsg = 4326)
+gdf = gdf.dissolve()
 
 coords_extration = coords_extration.drop(columns = ['index_right'])
 coords_extration = coords_extration.sjoin_nearest(gdf, distance_col = "park_dist")
@@ -129,140 +135,149 @@ coords_extration = coords_extration.sjoin_nearest(gdf, distance_col = "park_dist
 
 ### welfare facility
 aimFolder = "F:\\17_Article\\01_Data\\07_PointOfInterest\\welfare"
-try:
-    os.mkdir(aimFolder)
-except:
-    pass
-os.mkdir(aimFolder + "\\temp")
 
-### welfare
-### change the default path
-chromeOptions = webdriver.ChromeOptions()
-prefs = {"download.default_directory" : aimFolder + "\\temp"}
-chromeOptions.add_experimental_option("prefs",prefs)
-
-driver = webdriver.Chrome(ChromeDriverManager(version="100.0.4896.60").install(), chrome_options = chromeOptions)
-
-locationService = "https://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-P14.html"
-driver.get(locationService)
-
-# /html/body/main/div/table/tbody/tr[2]/td[6]/a
-# /html/body/main/div/table/tbody/tr[3]/td[6]/a
-# ...
-# /html/body/main/div/table/tbody/tr[94]/td[6]/a
-
-### download the zip file
-beginIndex = 2
-
-while beginIndex < 95:
-    driver.find_element_by_xpath("/html/body/main/div/table/tbody/tr[" + str(beginIndex) + "]/td[6]/a").click()
-    time.sleep(2)
+if firstTimeRunCode:
+    try:
+        os.mkdir(aimFolder)
+    except:
+        pass
+    os.mkdir(aimFolder + "\\temp")
     
-    driver.switch_to.alert.accept()
-    time.sleep(2)
+    ### welfare
+    ### change the default path
+    chromeOptions = webdriver.ChromeOptions()
+    prefs = {"download.default_directory" : aimFolder + "\\temp"}
+    chromeOptions.add_experimental_option("prefs",prefs)
     
-    beginIndex += 1
-
-driver.quit()
-
-### unzip downloaded files
-fileList = glob.glob(aimFolder + "\\temp" + "\\P14-15*.zip")
-fileAdd = glob.glob(aimFolder + "\\temp" + "\\P14-11_27*.zip")
-fileList = fileList.append(fileAdd[0])
-for filename in fileList:
-    with zipfile.ZipFile(filename, "r") as zip_ref:
-        zip_ref.extractall(aimFolder + "\\temp")
+    driver = webdriver.Chrome(ChromeDriverManager(version="100.0.4896.60").install(), chrome_options = chromeOptions)
+    
+    locationService = "https://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-P14.html"
+    driver.get(locationService)
+    
+    # /html/body/main/div/table/tbody/tr[2]/td[6]/a
+    # /html/body/main/div/table/tbody/tr[3]/td[6]/a
+    # ...
+    # /html/body/main/div/table/tbody/tr[94]/td[6]/a
+    
+    ### download the zip file
+    beginIndex = 2
+    
+    while beginIndex < 95:
+        driver.find_element_by_xpath("/html/body/main/div/table/tbody/tr[" + str(beginIndex) + "]/td[6]/a").click()
+        time.sleep(2)
         
-### read the shapefile        
-shapeFileList = glob.glob(aimFolder + "\\temp" + "\\*.shp")
-gdpFileArray = []
-for shapeFileName in shapeFileList:
-    gdpFile = gpd.read_file(shapeFileName)
-    gdpFileArray.append(gdpFile)
-gdf = gpd.GeoDataFrame(pd.concat(gdpFileArray))
-gdf = gdf[['geometry']]
-gdf.to_file(aimFolder + "\\MergedWelfare.shp")
-
-### remove the temp folder 
-for root, dirs, files in os.walk(aimFolder + "\\temp", topdown=False):
-    for name in files:
-        os.remove(os.path.join(root, name))
-    for name in dirs:
-        os.rmdir(os.path.join(root, name))
-os.rmdir(aimFolder + "\\temp")
+        driver.switch_to.alert.accept()
+        time.sleep(2)
+        
+        beginIndex += 1
+    
+    driver.quit()
+    
+    ### unzip downloaded files
+    fileList = glob.glob(aimFolder + "\\temp" + "\\P14-15*.zip")
+    fileAdd = glob.glob(aimFolder + "\\temp" + "\\P14-11_27*.zip")
+    fileList = fileList.append(fileAdd[0])
+    for filename in fileList:
+        with zipfile.ZipFile(filename, "r") as zip_ref:
+            zip_ref.extractall(aimFolder + "\\temp")
+            
+    ### read the shapefile        
+    shapeFileList = glob.glob(aimFolder + "\\temp" + "\\*.shp")
+    gdpFileArray = []
+    for shapeFileName in shapeFileList:
+        gdpFile = gpd.read_file(shapeFileName)
+        gdpFileArray.append(gdpFile)
+    gdf = gpd.GeoDataFrame(pd.concat(gdpFileArray))
+    gdf = gdf[['geometry']]
+    gdf.to_file(aimFolder + "\\MergedWelfare.shp")
+    
+    ### remove the temp folder 
+    for root, dirs, files in os.walk(aimFolder + "\\temp", topdown=False):
+        for name in files:
+            os.remove(os.path.join(root, name))
+        for name in dirs:
+            os.rmdir(os.path.join(root, name))
+    os.rmdir(aimFolder + "\\temp")
+else:
+    gdf = gpd.read_file(aimFolder + "\\MergedWelfare.shp")
 
 gdf.crs
 gdf = gdf.set_crs(epsg = 6668)
 gdf = gdf.to_crs(epsg = 4326)
+gdf = gdf.dissolve()
 
 coords_extration = coords_extration.drop(columns = ['index_right'])
 coords_extration = coords_extration.sjoin_nearest(gdf, distance_col = "welfare_dist")
 
 ### fire station
 aimFolder = "F:\\17_Article\\01_Data\\07_PointOfInterest\\fireStation"
-try:
-    os.mkdir(aimFolder)
-except:
-    pass
-os.mkdir(aimFolder + "\\temp")
-
-### fire station
-### change the default path
-chromeOptions = webdriver.ChromeOptions()
-prefs = {"download.default_directory" : aimFolder + "\\temp"}
-chromeOptions.add_experimental_option("prefs",prefs)
-
-driver = webdriver.Chrome(ChromeDriverManager(version="100.0.4896.60").install(), chrome_options = chromeOptions)
-
-locationService = "https://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-P17.html"
-driver.get(locationService)
-
-# /html/body/main/div/table/tbody/tr[2]/td[6]/a
-# /html/body/main/div/table/tbody/tr[3]/td[6]/a
-# ...
-# /html/body/main/div/table/tbody/tr[48]/td[6]/a
-
-### download the zip file
-beginIndex = 2
-
-while beginIndex < 49:
-    driver.find_element_by_xpath("/html/body/main/div/table/tbody/tr[" + str(beginIndex) + "]/td[6]/a").click()
-    time.sleep(2)
+if firstTimeRunCode:
+    try:
+        os.mkdir(aimFolder)
+    except:
+        pass
+    os.mkdir(aimFolder + "\\temp")
     
-    driver.switch_to.alert.accept()
-    time.sleep(2)
+    ### fire station
+    ### change the default path
+    chromeOptions = webdriver.ChromeOptions()
+    prefs = {"download.default_directory" : aimFolder + "\\temp"}
+    chromeOptions.add_experimental_option("prefs",prefs)
     
-    beginIndex += 1
-
-driver.quit()
-
-### unzip downloaded files
-fileList = glob.glob(aimFolder + "\\temp" + "\\*.zip")
-for filename in fileList:
-    with zipfile.ZipFile(filename, "r") as zip_ref:
-        zip_ref.extractall(aimFolder + "\\temp")
+    driver = webdriver.Chrome(ChromeDriverManager(version="100.0.4896.60").install(), chrome_options = chromeOptions)
+    
+    locationService = "https://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-P17.html"
+    driver.get(locationService)
+    
+    # /html/body/main/div/table/tbody/tr[2]/td[6]/a
+    # /html/body/main/div/table/tbody/tr[3]/td[6]/a
+    # ...
+    # /html/body/main/div/table/tbody/tr[48]/td[6]/a
+    
+    ### download the zip file
+    beginIndex = 2
+    
+    while beginIndex < 49:
+        driver.find_element_by_xpath("/html/body/main/div/table/tbody/tr[" + str(beginIndex) + "]/td[6]/a").click()
+        time.sleep(2)
         
-### read the shapefile        
-shapeFileList = glob.glob(aimFolder + "\\temp" + "\\*Station.shp")
-gdpFileArray = []
-for shapeFileName in shapeFileList:
-    gdpFile = gpd.read_file(shapeFileName)
-    gdpFileArray.append(gdpFile)
-gdf = gpd.GeoDataFrame(pd.concat(gdpFileArray))
-gdf = gdf[['geometry']]
-gdf.to_file(aimFolder + "\\MergedFirestation.shp")
-
-### remove the temp folder 
-for root, dirs, files in os.walk(aimFolder + "\\temp", topdown=False):
-    for name in files:
-        os.remove(os.path.join(root, name))
-    for name in dirs:
-        os.rmdir(os.path.join(root, name))
-os.rmdir(aimFolder + "\\temp")
+        driver.switch_to.alert.accept()
+        time.sleep(2)
+        
+        beginIndex += 1
+    
+    driver.quit()
+    
+    ### unzip downloaded files
+    fileList = glob.glob(aimFolder + "\\temp" + "\\*.zip")
+    for filename in fileList:
+        with zipfile.ZipFile(filename, "r") as zip_ref:
+            zip_ref.extractall(aimFolder + "\\temp")
+            
+    ### read the shapefile        
+    shapeFileList = glob.glob(aimFolder + "\\temp" + "\\*Station.shp")
+    gdpFileArray = []
+    for shapeFileName in shapeFileList:
+        gdpFile = gpd.read_file(shapeFileName)
+        gdpFileArray.append(gdpFile)
+    gdf = gpd.GeoDataFrame(pd.concat(gdpFileArray))
+    gdf = gdf[['geometry']]
+    gdf.to_file(aimFolder + "\\MergedFirestation.shp")
+    
+    ### remove the temp folder 
+    for root, dirs, files in os.walk(aimFolder + "\\temp", topdown=False):
+        for name in files:
+            os.remove(os.path.join(root, name))
+        for name in dirs:
+            os.rmdir(os.path.join(root, name))
+    os.rmdir(aimFolder + "\\temp")
+else:
+    gdf = gpd.read_file(aimFolder + "\\MergedFirestation.shp")
 
 gdf.crs
 gdf = gdf.set_crs(epsg = 6668)
 gdf = gdf.to_crs(epsg = 4326)
+gdf = gdf.dissolve()
 
 coords_extration = coords_extration.drop(columns = ['index_right'])
 coords_extration = coords_extration.sjoin_nearest(gdf, distance_col = "firestation_dist")
@@ -280,6 +295,7 @@ gdf = gpd.read_file(fileList[0])
 
 gdf.crs
 gdf = gdf.to_crs(epsg = 4326)
+gdf = gdf.dissolve()
 
 coords_extration = coords_extration.drop(columns = ['index_right'])
 coords_extration = coords_extration.sjoin_nearest(gdf, distance_col = "postOffice_dist")
