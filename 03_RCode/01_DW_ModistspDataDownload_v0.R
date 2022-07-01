5523ff45768c8b3ffcb445a7dd77d7d87cb5e201
Chao Li <chaoli0394@gmail.com>
2022-07-01 11:22:13 +0900

    O: set download code; create R 01_DW; done

---
+++ 03_RCode/01_DW_ModistspDataDownload_v0.R
@@ -0,0 +1,16 @@
+library(MODIStsp)
+
+#### NDVI
+MODIStsp_get_prodlayers("M*D13C2")
+
+MODIStsp(
+  gui = F, out_folder = "F:/17_Article/01_Data/16_NDVI",
+  out_folder_mod = "F:/17_Article/01_Data/16_NDVI",
+  selprod = "Vegetation_Indexes_Monthly_005dg (M*D13C2)", bandsel = "NDVI",
+  user = "chaoli0394", password = "097680Li",
+  start_date = "2001.01.01", end_date = "2020.12.31",
+  spatmeth= "file", spafile= "F:/15_Article/01_RawData/theWholeJapan.shp",
+  out_projsel = 'Native', output_proj = "MODIS Sinusoidal",
+  delete_hdf = T, out_format = "GTiff",
+  parallel = 8
+)
