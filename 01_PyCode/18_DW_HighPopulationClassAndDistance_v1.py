296affd7a377b4cc37ea79ee75485c7eb7250df8
Chao Li <chaoli0394@gmail.com>
2022-07-06 10:25:39 +0900

    O: finish high populaiton density dummy and distance; revise 18_DW; done

--- 01_PyCode/18_DW_HighPopulationClassAndDistance_v1.py
+++ 01_PyCode/18_DW_HighPopulationClassAndDistance_v1.py
@@ -84,6 +84,9 @@ with2000HighPopDensity = with2000HighPopDensity.drop(columns='index_right')
 dist2000HighPopDensity = pd.DataFrame(dist2000HighPopDensity.drop(columns=['index_right', 'geometry'])).set_index('G04c_001')
 HighPopDensity2000 = pd.concat([dist2000HighPopDensity, with2000HighPopDensity], axis=1)
 HighPopDensity2000 = HighPopDensity2000.fillna(0)
+HighPopDensity2000['year'] = 2000
+HighPopDensity2000.reset_index(inplace = True)
+HighPopDensity2000 = HighPopDensity2000.set_index(['G04c_001', 'year'])
 
 ### 2005
 ### read the shapefile     
@@ -105,27 +108,65 @@ with2005HighPopDensity = with2005HighPopDensity.drop(columns='index_right')
 dist2005HighPopDensity = pd.DataFrame(dist2005HighPopDensity.drop(columns=['index_right', 'geometry'])).set_index('G04c_001')
 HighPopDensity2005 = pd.concat([dist2005HighPopDensity, with2005HighPopDensity], axis=1)
 HighPopDensity2005 = HighPopDensity2005.fillna(0)
+HighPopDensity2005['year'] = 2005
+HighPopDensity2005.reset_index(inplace = True)
+HighPopDensity2005 = HighPopDensity2005.set_index(['G04c_001', 'year'])
 
+### 2010
+gdf_2010 = gpd.read_file("F:/17_Article/01_Data/06_HighDensityPopulation/A16-10_GML/A16-10_00_DID.shp")
+gdf_2010 = gdf_2010[['geometry']].dissolve().to_crs(epsg = 4326)
+
+with2010HighPopDensity = coords_extration.set_crs(epsg = 4326).sjoin(gdf_2010)
+dist2010HighPopDensity = coords_extration.set_crs(epsg = 4326).sjoin_nearest(gdf_2010, distance_col = "dist")
+with2010HighPopDensity = pd.DataFrame(with2010HighPopDensity.drop(columns='geometry')).set_index('G04c_001')
+with2010HighPopDensity['hidhPopDensity'] = 1
+with2010HighPopDensity = with2010HighPopDensity.drop(columns='index_right')
+dist2010HighPopDensity = pd.DataFrame(dist2010HighPopDensity.drop(columns=['index_right', 'geometry'])).set_index('G04c_001')
+HighPopDensity2010 = pd.concat([dist2010HighPopDensity, with2010HighPopDensity], axis=1)
+HighPopDensity2010 = HighPopDensity2010.fillna(0)
+HighPopDensity2010['year'] = 2010
+HighPopDensity2010.reset_index(inplace = True)
+HighPopDensity2010 = HighPopDensity2010.set_index(['G04c_001', 'year'])
+
+### 2015
+gdf_2015 = gpd.read_file("F:/17_Article/01_Data/06_HighDensityPopulation/A16-15_GML/A16-15_00_DID.shp")
+gdf_2015 = gdf_2015.buffer(0)
+gdf_2015 = gpd.GeoDataFrame(geometry = gdf_2015, crs="EPSG:4326")
+gdf_2015 = gdf_2015[['geometry']].dissolve().to_crs(epsg = 4326)
+
+with2015HighPopDensity = coords_extration.set_crs(epsg = 4326).sjoin(gdf_2015)
+dist2015HighPopDensity = coords_extration.set_crs(epsg = 4326).sjoin_nearest(gdf_2015, distance_col = "dist")
+with2015HighPopDensity = pd.DataFrame(with2015HighPopDensity.drop(columns='geometry')).set_index('G04c_001')
+with2015HighPopDensity['hidhPopDensity'] = 1
+with2015HighPopDensity = with2015HighPopDensity.drop(columns='index_right')
+dist2015HighPopDensity = pd.DataFrame(dist2015HighPopDensity.drop(columns=['index_right', 'geometry'])).set_index('G04c_001')
+HighPopDensity2015 = pd.concat([dist2015HighPopDensity, with2015HighPopDensity], axis=1)
+HighPopDensity2015 = HighPopDensity2015.fillna(0)
+HighPopDensity2015['year'] = 2015
+HighPopDensity2015.reset_index(inplace = True)
+HighPopDensity2015 = HighPopDensity2015.set_index(['G04c_001', 'year'])
+
+#### previous code, discarded
 ### extraction
-coords_extration = gpd.read_file("F:/17_Article/01_Data/00_mesh/mesh_center_point.shp")
+#coords_extration = gpd.read_file("F:/17_Article/01_Data/00_mesh/mesh_center_point.shp")
 
-highDensityShapeFile = gpd.read_file("F:/17_Article/01_Data/06_HighDensityPopulation/A16-15_GML/A16-15_00_DID.shp")
-highDensityShapeFile = highDensityShapeFile.set_crs(epsg = 4326)
-highDensityShapeFile = highDensityShapeFile[['geometry']]
-highDensityShapeFileRepair = highDensityShapeFile.buffer(0)
-highDensityShapeFile = gpd.GeoDataFrame(geometry = highDensityShapeFileRepair, crs="EPSG:4326")
-highDensityShapeFileDissolved = highDensityShapeFile.dissolve()
-highDensityShapeFileDissolved['within'] = 1
+#highDensityShapeFile = gpd.read_file("F:/17_Article/01_Data/06_HighDensityPopulation/A16-15_GML/A16-15_00_DID.shp")
+#highDensityShapeFile = highDensityShapeFile.set_crs(epsg = 4326)
+#highDensityShapeFile = highDensityShapeFile[['geometry']]
+#highDensityShapeFileRepair = highDensityShapeFile.buffer(0)
+#highDensityShapeFile = gpd.GeoDataFrame(geometry = highDensityShapeFileRepair, crs="EPSG:4326")
+#highDensityShapeFileDissolved = highDensityShapeFile.dissolve()
+#highDensityShapeFileDissolved['within'] = 1
 
-joinShape = coords_extration.sjoin(highDensityShapeFileDissolved)
-joinDistance = coords_extraction.sjoin_nearest(highDensityShapeFileDissolved, distance_col = "dist")
-joinDistance = joinDistance.drop(columns = ["within"]) 
+#joinShape = coords_extration.sjoin(highDensityShapeFileDissolved)
+#joinDistance = coords_extraction.sjoin_nearest(highDensityShapeFileDissolved, distance_col = "dist")
+#joinDistance = joinDistance.drop(columns = ["within"]) 
 
-joinShape = joinShape[["id", "within"]]
+#joinShape = joinShape[["id", "within"]]
 
-result = pd.merge(joinDistance, joinShape, on='id', how='left')
-result['within'] = result['within'].fillna(0)
-result = result.drop(columns = ["index_right"]) 
+#result = pd.merge(joinDistance, joinShape, on='id', how='left')
+#result['within'] = result['within'].fillna(0)
+#result = result.drop(columns = ["index_right"]) 
 
 result.loc[result['within'] == 1, 'dist'] = 0
 result.to_pickle("F:/17_Article/01_Data/99_MiddleFileStation/07_PopulationDensityClass.pkl")
